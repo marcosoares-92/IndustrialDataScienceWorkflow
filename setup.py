9ced225355c4fbac3041e9cb5689e24f7fdc18ff
Marco Cesar Prado Soares <80632300+marcosoares-92@users.noreply.github.com>
2022-07-22 16:25:38 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ import setuptools
 
 long_description = """
 Industrial Data Science Workflow: full workflow for ETL, statistics, and Machine learning modelling of (usually) time-stamped industrial facilities data.
+
+Check the project Github: https://github.com/marcosoares-92/IndustrialDataScienceWorkflow
 """
 with open("requirements.txt", "r") as file:
     required_packages = [package.strip() for package in file.readlines()]
 
 setuptools.setup(
     name="idsw",
-    version="1.0.2",
+    version="1.0.3",
     author="Marco César Prado Soares, Gabriel Fernandes Luz",
     author_email="marcosoares.feq@gmail.com, gfluz94@gmail.com",
     description=long_description,
